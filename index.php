2d25e1ce83f7da4bf6cd261815463bdf5da059e0
MatthewTKa <mat.17.01@icloud.com>
2021-03-06 14:16:53 +0700

    show-Content

--- index.php
+++ index.php
@@ -1,26 +1,19 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/header.php';
+if (isset($_GET['page'])) {
 
-$file = $_SERVER['DOCUMENT_ROOT'] . '/file.php';
-if(file_exists($file)){
-    include $file;
-}
-else{
-    echo 'Файла не существует';
-}
+    $page = $_SERVER['DOCUMENT_ROOT'] . '/pages/' . $_GET['page'] . '.php';
 
-$dir = scandir($_SERVER['DOCUMENT_ROOT'] . '/pages/');
-$dir = array_values(array_diff($dir, ['.', '..']));
+    if (file_exists($page)) {
+        $content = file_get_contents($page);
+    } else {
+        $content = 'Файла не существует';
+    }
+}
 
 ?>
 <main>
-<?php
-if(isset($_GET['page'])){
-    $page = $_GET['page'];
-    include $_SERVER['DOCUMENT_ROOT'] . '/pages/'.$page.'.php';
-}
-?>
-test
+    <?= $content; ?>
 </main>
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . '/footer.php';
